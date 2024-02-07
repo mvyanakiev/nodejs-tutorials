97d254e404a956952e7064f9343aff4bcd4edc20
Milko Yanakiev <myanakiev@gmail.com>
2024-02-07 16:47:49 +0200

    update

--- index.js
+++ index.js
@@ -1,24 +1,39 @@
 import express from 'express'
 const app = express()
 
-app.use(express.json())
+app.use(express.json()) // enable json deserialising
 
-app.get("/", (req, res) => {
-    // console.log(req)
-
+app.use((req, res, next) => {
+    console.log('middleware called') // Изпълнява се преди метода за ендпоинта. Филтри, security и др.
     res.json({
-        "hi": "hi"
+        "used method": req.method
     })
-
+    next() // предава изпълнението нататък
 })
 
-app.get("/", (req, res) => {
-    // console.log(req)
 
+
+app.get("/", (req, res) => {
     res.json({
         "hi": "hi"
     })
+})
 
+app.all("/all", (req, res) => { // ALL methods Post, Get..
+    console.log(req.method)
+    res.json({
+        "key": "ALL"
+    })
+})
+
+app.post("/parameter/:xyz", (req, res) => { // curl -X POST 'localhost:3000/123'
+    console.log(req.params) // { param: '123' }
+    console.log(req.params.xyz) // 123
+})
+
+app.post("/query", (req, res) => { // curl -X POST 'localhost:3000/query?abc=123&xyz=456'
+    console.log(req.query) // { abc: '123', xyz: '456' }
+    console.log(req.query.xyz) // 456
 })
 
 app.listen(3000, () => {
